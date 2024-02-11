2fe8f73d49e790007a2e6d0dd9de8afee2cce039
Keine Secrets <miethe.max@web.de>
2024-02-11 21:09:41 +0100

    Added KeyHandler and events

--- src/main/java/dev/mzcy/keys/KeyHandler.java
+++ src/main/java/dev/mzcy/keys/KeyHandler.java
@@ -6,16 +6,16 @@ import java.awt.event.KeyListener;
 public class KeyHandler implements KeyListener {
     @Override
     public void keyTyped(KeyEvent e) {
-
+        KeyCode keyCode = KeyCode.fromValue(e.getKeyCode());
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-
+        KeyCode keyCode = KeyCode.fromValue(e.getKeyCode());
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-
+        KeyCode keyCode = KeyCode.fromValue(e.getKeyCode());
     }
 }
