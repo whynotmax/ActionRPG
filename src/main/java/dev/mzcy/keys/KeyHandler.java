45c775204e3085264ad6ad34ecb3f168babb6bb5
Keine Secrets <miethe.max@web.de>
2024-02-12 11:22:23 +0100

    Added delta method game loop

--- src/main/java/dev/mzcy/keys/KeyHandler.java
+++ src/main/java/dev/mzcy/keys/KeyHandler.java
@@ -4,18 +4,45 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener {
+
+    public boolean up, down, left, right;
+
     @Override
     public void keyTyped(KeyEvent e) {
-        KeyCode keyCode = KeyCode.fromValue(e.getKeyCode());
+
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
         KeyCode keyCode = KeyCode.fromValue(e.getKeyCode());
+        if (keyCode == KeyCode.W || keyCode == KeyCode.UP_ARROW) {
+            up = true;
+        }
+        if (keyCode == KeyCode.A || keyCode == KeyCode.LEFT_ARROW) {
+            left = true;
+        }
+        if (keyCode == KeyCode.S || keyCode == KeyCode.DOWN_ARROW) {
+            down = true;
+        }
+        if (keyCode == KeyCode.D || keyCode == KeyCode.RIGHT_ARROW) {
+            right = true;
+        }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
         KeyCode keyCode = KeyCode.fromValue(e.getKeyCode());
+        if (keyCode == KeyCode.W || keyCode == KeyCode.UP_ARROW) {
+            up = false;
+        }
+        if (keyCode == KeyCode.A || keyCode == KeyCode.LEFT_ARROW) {
+            left = false;
+        }
+        if (keyCode == KeyCode.S || keyCode == KeyCode.DOWN_ARROW) {
+            down = false;
+        }
+        if (keyCode == KeyCode.D || keyCode == KeyCode.RIGHT_ARROW) {
+            right = false;
+        }
     }
 }
--- src/main/java/dev/mzcy/panel/GamePanel.java
+++ src/main/java/dev/mzcy/panel/GamePanel.java
@@ -1,5 +1,7 @@
 package dev.mzcy.panel;
 
+import dev.mzcy.keys.KeyHandler;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -13,8 +15,13 @@ public class GamePanel extends JPanel implements Runnable {
     private static final int MAX_SCREEN_ROW = 12;
     private static final int SCREEN_WIDTH = ACTUAL_TILE_SIZE * MAX_SCREEN_COL;  //768px
     private static final int SCREEN_HEIGHT = ACTUAL_TILE_SIZE * MAX_SCREEN_ROW; //576px
+    private static final int FPS = 60;
 
     Thread gameThread;
+    KeyHandler keyHandler = new KeyHandler();
+
+    //PLAYER'S DEFAULT POS
+    int playerX = 100, playerY = 100, playerSpeed = 4;
 
     public GamePanel() {
 
@@ -22,6 +29,8 @@ public class GamePanel extends JPanel implements Runnable {
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
 
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
 
     }
 
@@ -34,22 +43,45 @@ public class GamePanel extends JPanel implements Runnable {
     @Override
     public void run() {
 
+        double drawInterval = (double) 1000000000 /FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
         while (gameThread != null) {
-            update();
-            repaint();
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
         }
 
     }
 
     public void update() {
-
+        if (keyHandler.up) {
+            playerY -= playerSpeed;
+        }
+        if (keyHandler.down) {
+            playerY += playerSpeed;
+        }
+        if (keyHandler.left) {
+            playerX -= playerSpeed;
+        }
+        if (keyHandler.right) {
+            playerX += playerSpeed;
+        }
     }
 
     public void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
         Graphics2D graphics2D = (Graphics2D) graphics;
         graphics2D.setColor(Color.white);
-        graphics2D.fillRect(100, 100, ACTUAL_TILE_SIZE, ACTUAL_TILE_SIZE);
+        graphics2D.fillRect(playerX, playerY, ACTUAL_TILE_SIZE, ACTUAL_TILE_SIZE);
         graphics2D.dispose();
     }
 
